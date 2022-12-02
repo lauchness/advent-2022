c4a362ad3fec17abb68d11836fd46ae619f5269b
lauchness <lauchlanchisholm@gmail.com>
2022-12-02 10:57:06 -0400

    solution for day-2 part 2

--- solutions/day-2/index.ts
+++ solutions/day-2/index.ts
@@ -4,9 +4,9 @@ const rpsMap = {
   A: 1,
   B: 2,
   C: 3,
-  X: 1,
-  Y: 2,
-  Z: 3,
+  X: 0,
+  Y: 3,
+  Z: 6,
 };
 
 type mappingKey = keyof typeof rpsMap;
@@ -29,28 +29,28 @@ fs.readFile("input.txt", "utf8", (err, data) => {
   const rounds = data.split("\n");
   const finalScore = rounds.reduce((score, round, i) => {
     const opponent = getRpsMapping(round[0]);
-    const me = getRpsMapping(round[2]);
+    const result = getRpsMapping(round[2]);
 
     const validOpponent = typeof opponent === "number";
-    const validMe = typeof me === "number";
+    const validResult = typeof result === "number";
 
-    if (!validOpponent || !validMe) {
+    if (!validOpponent || !validResult) {
       throw new Error(
-        `Input Error for round ${i} - opponent: ${opponent} me: ${me}`
+        `Input Error for round ${i} - opponent: ${opponent} result: ${result}`
       );
     }
 
-    const result = me - opponent;
-
-    if (result === -2 || result === 1) {
-      return score + 6 + me;
+    let me = 0;
+    if (result === 6) {
+      me = (opponent % 3) + 1;
     }
-
+    if (result === 3) {
+      me = opponent;
+    }
     if (result === 0) {
-      return score + 3 + me;
+      me = ((opponent + 1) % 3) + 1;
     }
-
-    return score + me;
+    return score + result + me;
   }, 0);
   console.log("final score", finalScore);
 });
